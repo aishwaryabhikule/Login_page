4e77ac4da03a7cf7af83bb0d642af0f18134062d
aishwaryabhikule <78999634+aishwaryabhikule@users.noreply.github.com>
2023-04-24 19:17:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var username = document.forms['myform']['username'];
-var email = document.forms['myform']['email'];
+var username = document.getElementById('fname');
+var email = document.getElementById('email');
 
 var PANNumber = document.getElementById("PANNumber");
 var PANCard = document.getElementById("PANCard");
@@ -9,54 +9,79 @@ var regx = /^[a-zA-Z ]*$/;
 var pattern = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
 var regex =  /([A-Z]){5}([0-9]){4}([A-Z]){1}$/;  
 
+username.onblur = function() {myfunction()};
+email.onblur = function() {myfunction1()};
+PANNumber.onblur = function() {validation()};
 
-
-
-function validation(){
+function myfunction(){
     if(!username.value.match(regx)){
-       
         error.textContent = "Name must content characters";
         error.style.color = "red";
+       
         return false;
     }
+    error.textContent = " ";
+    return true;
+}
 
-    else if(!email.value.match(pattern)){
+function myfunction1(){  
+    if(!email.value.match(pattern)){
         
         error1.textContent = "Invalid Email id";
         error1.style.color = "red";
         return false;
     }
+    error1.textContent = " ";
+    return true;
+}
+
+
+
+function validation(){
+    // if(!username.value.match(regx)){
+    //     error.textContent = "Name must content characters";
+    //     error.style.color = "red";
+    //     return false;
+    // }
+
+    //  if(!email.value.match(pattern)){
+        
+    //     error1.textContent = "Invalid Email id";
+    //     error1.style.color = "red";
+    //     return false;
+    // }
     
-    else {
-        if (regex.test(PANNumber.value.toUpperCase())) {
+    if (regex.test(PANNumber.value.toUpperCase())) {
             PANCard.style.visibility = "hidden";
-            
+       
         } 
         else {
             PANCard.style.visibility = "visible";
             return false;
         }
-        PANCard.textContent = "";
-       
-    }
+        // PANCard.textContent = "";
     
     sessionStorage.setItem('fname',username.value);
     sessionStorage.setItem('email',email.value);
-   
-return true;
+    PANCard.textContent = " ";
+    return true;
+    }
 
-
-}
 
 function calculate(){
-    amount=document.getElementById('amount').value;
-    rate=document.getElementById('rate').value;
-    time=document.getElementById('time').value;
-    const interest = (amount * (rate * 0.01)) / time;
-    let emi = ((amount / time) + interest).toFixed(2);
-    emi = emi.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    document.getElementById("output").innerHTML = emi;
+
+    var amount = document.getElementById('amount').value;
+    var ir = 8.5 /12/ 100;
+    var nm = 15 * 12;
+    var rate = Math.pow((ir+1),nm);
+
+    var emi = amount * ir * (rate) / (rate-1);
+    console.log(amount);
+    console.log(rate);
+    console.log(emi);
+    
+    
+    document.getElementById("output").innerHTML = parseInt(emi);
 }
 
-
-
+ 
